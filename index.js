e4bdcd186fe817a7ee56ca462c4c4cc33b8518b0
nicomiguelino <nicomiguelino2014@gmail.com>
2021-05-14 13:53:33 +0800

    Create an endpoint for getting the list of all tweets.
    - Hardcoded values are returned for this endpoint.
    - Database usage is not yet supported.

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async() => {
   const server = Hapi.server({
-    port: 3000,
+    port: 4000,
     host: 'localhost',
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      const data = {
-        message: 'Hello, World 2.0!',
-      };
-
-      return h.response(data).code(200);
+    routes: {
+      cors: {
+        origin: ['*']
+      }
     }
   });
+
+  server.route(routes);
 
   await server.start();
 
---
+++ routes/index.js
@@ -0,0 +1,47 @@
+const tweets = [
+  {
+    userName: 'anthonyedwardstark',
+    displayName: 'Tony Stark',
+    timeElapsed: '4h',
+    content: 'Time to upgrade my Mark V.',
+  },
+  {
+    userName: 'peterparker',
+    displayName: 'Peter Parker',
+    timeElapsed: '23s',
+    content: 'I\'m super excited on my first day at the Stark internship.',
+  },
+  {
+    userName: 'brucebanner',
+    displayName: 'Hulk',
+    timeElapsed: '4h',
+    content: 'Gotta go to dinner date with Nat.',
+  },
+  {
+    userName: 'samwilson',
+    displayName: 'The Falcon',
+    timeElapsed: '1d',
+    content: 'Gotta go fast to get that shield back.',
+  },
+]
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      const data = {
+        message: 'Hello, World 2.0!',
+      };
+
+      return h.response(data).code(200);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/tweets',
+    handler: (request, h) => {
+      return h.response(tweets).code(200);
+    }
+  }
+];
